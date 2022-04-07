6ced57bd0f381626b8b0158777f511d602e4fa59
Leonardo Uieda <leouieda@gmail.com>
2022-04-07 17:30:40 +0100

    Move functions to a printing module (#16)
    Move code and refactor some into functions

---
+++ nene/printing.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2021 Leonardo Uieda.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+"""Utilities for printing things with the Rich Console."""
+
+import rich.console
+
+
+def make_console(verbose):
+    """
+    Start up the :class:`rich.console.Console` instance we'll use.
+
+    Parameters
+    ----------
+    verbose : bool
+        Whether or not to print status messages to stderr.
+    """
+    return rich.console.Console(stderr=True, quiet=not verbose, highlight=False)
+
+
+def print_dict(dictionary, console):
+    """
+    Print the key/value pairs of a dictionary one per line.
+
+    Parameters
+    ----------
+    dictionary : dict
+        The dict.
+    console : rich.console.Console
+        The console used for printing.
+    """
+    for key in sorted(dictionary):
+        value = dictionary[key]
+        if isinstance(value, str):
+            value = " ".join([part.strip() for part in value.split("\n")])
+        console.print(f"   {key}: {value}", highlight=False)
+
+
+def print_file_stats(files, console):
+    """
+    Print the number of files of each type.
+
+    Parameters
+    ----------
+    files : dict
+        Dictionary with lists of files separated by file type.
+    console : rich.console.Console
+        The console used for printing.
+    """
+    line_length = max(len(key) for key in files)
+    for file_type in files:
+        file_type_print = " " * (line_length - len(file_type)) + file_type
+        console.print(f"   {file_type_print} = {len(files[file_type])}")
