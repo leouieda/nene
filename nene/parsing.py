29ac2ab386775a1fcd104017f672c2c36101de2a
Florian Wagner <mail@fwagner.info>
2023-02-02 18:44:16 +0100

    Apply new namespace convention for data parsers to bibtex parser

--- nene/parsing.py
+++ nene/parsing.py
@@ -115,7 +115,7 @@ def load_data(path):
     """
     identifier = generate_identifier(path)
     if bibtexparser is not None and path.suffix == ".bib":
-        content = {"bibtex_" + path.stem: load_bibtex(path)}
+        content = load_bibtex(path)
     else:
         content = _read_data_file(path)
     data = {
