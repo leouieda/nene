6b7c14a70469954a21574ec2e0d5a72238fee09b
Leonardo Uieda <leo@uieda.com>
2025-07-12 00:42:59 -0300

    Add support for syntax highlighting with pygments
    Runs the highlighter code on code blocks using the solution from
    https://github.com/executablebooks/markdown-it-py/issues/256#issuecomment-2937277893
    Doesn't add the stylesheet automatically. For highlighting to actually
    show up, users need to generate the pygments stylesheets (see example in
    the doc folder) and add it to the HTML.

---
+++ doc/get_pygments_stylesheet.py
@@ -0,0 +1,10 @@
+"""
+Generate a pygments CSS stylesheet.
+"""
+import pathlib
+import pygments.formatters
+
+
+css = pygments.formatters.HtmlFormatter(style="default").get_style_defs('.highlight')
+output = pathlib.Path("css") / "pygments.css"
+output.write_text(css)
